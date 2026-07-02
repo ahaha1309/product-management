a18cf45a525214ff9788419fb1dde88824bcaf5b
Nguyen Van Ha <nguyentieubao96@gmail.com>
2026-07-02 11:44:53 +0700

    fix error

--- controller/admin/chat.controller.js
+++ controller/admin/chat.controller.js
@@ -1,15 +1,31 @@
 const Chat = require('../../models/chat.model');
 const User = require('../../models/user.model');
 
+const mongoose = require('mongoose');
+
 module.exports.index = async (req, res) => {
   try {
     // Lấy ra danh sách các userId đã từng chat
     const uniqueUserIds = await Chat.distinct('userId', { deleted: false });
     
-    // Lấy thông tin user
-    const users = await User.find({ _id: { $in: uniqueUserIds }, deleted: false }).select('fullName avatar email');
+    // Tách riêng ID hợp lệ (user đăng nhập) và ID dạng chuỗi (khách ẩn danh)
+    const validUserIds = uniqueUserIds.filter(id => mongoose.Types.ObjectId.isValid(id));
+    const anonymousIds = uniqueUserIds.filter(id => !mongoose.Types.ObjectId.isValid(id));
     
-    // Có thể bổ sung lấy tin nhắn cuối cùng để hiển thị preview, nhưng làm đơn giản trước
+    // Lấy thông tin user đã đăng ký
+    const users = await User.find({ _id: { $in: validUserIds }, deleted: false })
+                            .select('fullName avatar email')
+                            .lean();
+                            
+    // Bổ sung khách ẩn danh vào danh sách hiển thị
+    anonymousIds.forEach(id => {
+      users.push({
+        _id: id,
+        fullName: 'Khách vãng lai (Chưa ĐN)',
+        email: 'Ẩn danh',
+        avatar: 'https://dummyimage.com/40x40/cbd5e1/ffffff'
+      });
+    });
     
     res.render('admin/pages/chat/index', {
       title: 'Hỗ trợ khách hàng',
